549306134ee1bac8b1f12cd58ff23d6c21c16c73
coolbuhzor <coolbuhzor@gmail.com>
2024-01-03 13:26:46 +0100

    ejs tutorial

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const express = require("express");
 // express app
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs");
+
 // listen for requests
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  //   res.send("<p>home page</p>");
-  //   console.log("testing express");
   res.sendFile("./lesson-3/views/index.html", { root: __dirname });
 });
 
 app.get("/about", (req, res) => {
-  //   res.send("<p>about page</p>");
-  //   console.log("testing express");
   res.sendFile("./lesson-3/views/about.html", { root: __dirname });
 });
 
