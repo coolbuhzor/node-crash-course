14275d8da42736d98f8e7910b347a8b12847490d
coolbuhzor <coolbuhzor@gmail.com>
2024-01-04 12:21:46 +0100

    dynamic data with ejs

--- app.js
+++ app.js
@@ -10,11 +10,29 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./lesson-3/views/index.html", { root: __dirname });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs: blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./lesson-3/views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new blog" });
 });
 
 // redirects
@@ -24,7 +42,6 @@ app.get("/about-us", (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res
-    .status(404)
-    .res.sendFile("./lesson-3/views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404 " });
+  // .res.sendFile("./lesson-3/views/404.html", { root: __dirname });
 });
